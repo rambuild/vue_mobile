0ed89259791fcffeef97912bc31afa9f875be6b7
rambuild <276822063@qq.com>
2019-11-22 18:34:11 +0800

    vscode push

--- src/components/home/newsList.vue
+++ src/components/home/newsList.vue
@@ -1,64 +1,62 @@
 <template>
   <div>
-  	<ul class="mui-table-view">
-		<li class="mui-table-view-cell mui-media" v-for="(item,index) in newsList">
-			<router-link :to="'/home/newsInfo/'+item.id">
-				<img class="mui-media-object mui-pull-left" src="../../../static/img/shuijiao.jpg">
-				<div class="mui-media-body">
-					<p>{{item.title}}</p>
-					<p class="mui-publishContent">
-						<span>发表时间：{{item.add_time | dateFormat}}</span><span>点击：{{item.click}}</span>
-					</p>
-				</div>
-			</router-link>
-		</li>
-	</ul>
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media" v-for="(item,index) in newsList" :key="item.id">
+        <router-link :to="'/home/newsInfo/'+item.id">
+          <img class="mui-media-object mui-pull-left" src="../../../static/img/shuijiao.jpg" />
+          <div class="mui-media-body">
+            <p>{{item.title}}</p>
+            <p class="mui-publishContent">
+              <span>发表时间：{{item.add_time | dateFormat}}</span>
+              <span>点击：{{item.click}}</span>
+            </p>
+          </div>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 export default {
-  data () {
+  data() {
     return {
-     newsList:[]
+      newsList: []
+    };
+  },
+  methods: {
+    getNewsList() {
+      this.$http.get("api/getnewsList").then(res => {
+        this.newsList = res.data.message;
+      });
     }
   },
-  methods:{
-  	getNewsList(){
-  		this.$http.get('api/getnewsList').then(res=>{
-  			this.newsList=res.data.message
-  		})
-  	}
-  	
-  },
-  created(){
-  	this.getNewsList();
+  created() {
+    this.getNewsList();
   }
-}
+};
 </script>
 
 
 <style lang="scss" scoped>
+@import "../../lib/mui/dist/css/mui.css";
 
-@import '../../lib/mui/dist/css/mui.css';
-
-.mui-media-body{
-	background-size: 10%;
-	text-decoration: none;
-	text-decoration:none;
-	p{		
-		font-size: 16px;
-		color:#000;
-		margin-bottom:5px;
-	}
-	span{
-		font-size:12px;
-		color:rgb(38,162,255);
-	}
-	.mui-publishContent{
-		display: flex;
-		justify-content: space-between;
-	}
+.mui-media-body {
+  background-size: 10%;
+  text-decoration: none;
+  text-decoration: none;
+  p {
+    font-size: 16px;
+    color: #000;
+    margin-bottom: 5px;
+  }
+  span {
+    font-size: 12px;
+    color: rgb(38, 162, 255);
+  }
+  .mui-publishContent {
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
